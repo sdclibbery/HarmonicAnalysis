536d52c9e3c1df4b991079d87a2b1175d16b607c
sdclibbery <sdclibbery@googlemail.com>
2014-09-29 23:13:54 +0100

    Add tests for rests and incidentals
    But disable incidentals until some more refactoring :-)

--- MelodyTests.hs
+++ MelodyTests.hs
@@ -21,10 +21,14 @@ b = beat B 4
 r = Beat Rest []
 
 testRuleH89 = TestLabel "ruleH89" $ TestList
-  [ test []                                                                $ Music [c, d]
+  [ test []                                                               $ Music [c, d]
+  , test []                                                               $ Music [c, r, b]
   , test [Error (Harmony 89) (Source 0 1) "Dissonance Major7"]            $ Music [c, b]
-  , test []                                                                $ Music [beat B 3, beat C 4]
+  , test [Error (Harmony 89) (Source 1 2) "Dissonance Major7"]            $ Music [r, c, b, r]
+  , test []                                                               $ Music [beat B 3, beat C 4]
   , test [Error (Harmony 89) (Source 0 1) "Dissonance -Major7"]           $ Music [b, c]
   , test [Error (Harmony 89) (Source 1 2) "Dissonance Major7"]            $ Music [d, c, b]
+--  , test [Error (Harmony 89) (Source 0 0) "Dissonance Major7"]            $ Music [Beat (Note $ C Nat 4) [B Nat 4]]
+--  , test [Error (Harmony 89) (Source 0 0) "Dissonance Major7"]            $ Music [Beat Rest [C Nat 4, B Nat 4]]
   ] where
     test e s = show s ~: e ~=? analyse s
--- RunTests.hs
+++ RunTests.hs
@@ -2,13 +2,13 @@ import Test.HUnit
 import qualified MelodyTests
 
 {- TODO:
+* DataStructures
+ * Input data is array of parts
+ * Can then get single parts or pairs of parts
+  * And in such a way that you can zipper through them 'in unison'
 * Rule 89
   * ?? Should test incidentals?? What about rests??
 * Do a harmonic test next
- * DataStructures
-  * Input data is array of parts
-  * Can then get single parts or pairs of parts
-   * And in such a way that you can zipper through them 'in unison'
 * Then back to Rule 90...
 -}
 
