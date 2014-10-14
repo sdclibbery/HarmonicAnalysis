80dc42fd8259fcd49a8ac72f1f253e22bf6fa8bf
sdclibbery <sdclibbery@googlemail.com>
2014-10-14 19:52:20 +0100

    First experiments with random composer

--- RunTests.hs
+++ RunTests.hs
@@ -3,9 +3,6 @@ import qualified MelodyTests
 import qualified HarmonyTests
 
 {- TODO:
-x Set up midi export and play
- x Duration
- x Rests
 * Set up a random composer
 * Set up a random but filtered composer
 * Continue with more rules
