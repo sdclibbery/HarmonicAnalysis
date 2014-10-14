744f87519b7547e84ff3a09c4f3f472347c712a3
sdclibbery <sdclibbery@googlemail.com>
2014-10-15 00:08:18 +0100

    Random composer picks random notes

--- RunTests.hs
+++ RunTests.hs
@@ -3,7 +3,6 @@ import qualified MelodyTests
 import qualified HarmonyTests
 
 {- TODO:
-x Switch away from having indivdual diatone constructors in Note
 * Set up a random composer
 * Set up a random but filtered composer
 * Continue with more rules
