95ad07049125a40d9681c2eaa13cbb69bb0c75e6
sdclibbery <sdclibbery@googlemail.com>
2014-10-15 23:51:51 +0100

    Replan

--- RunTests.hs
+++ RunTests.hs
@@ -3,7 +3,12 @@ import qualified MelodyTests
 import qualified HarmonyTests
 
 {- TODO:
+* Split Midi export into own file
 * Set up a random composer
+ * Durations should be inverse powers of two, or dotted versions only
+ * Can include rests
+ * Able to compose multiple until a given duration
+  * Each part has an appropriate pitch range
 * Set up a random but filtered composer
 * Continue with more rules
 -}
