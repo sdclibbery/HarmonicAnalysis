450427248c094dd61311774b7c0e367b8a913f9a
sdclibbery <sdclibbery@googlemail.com>
2014-10-26 21:04:42 +0000

    Tests for entire parts of consecutive unisons/octaves
    But don't support this yet because the code becomes untidy.

--- HarmonyTests.hs
+++ HarmonyTests.hs
@@ -15,20 +15,23 @@ tests = TestLabel "Harmony" $ TestList
 music' = music . map (.>> 4)
 
 testRuleH96 = TestLabel "ruleH96" $ TestList
-    [ test []                                                                      $ music' [ [c, d], [d, e] ]
-    , test []                                                                      $ music' [ [c, d], [c, e] ]
-    , test []                                                                      $ music' [ [c, d], [d, d] ]
-    , test [Error (Harmony 96) (Source ["p", "p2"] 0 2) "Consecutive unisons"]     $ music' [ [c, d], [c, d] ]
-    , test []                                                                      $ music' [ [c, d], [c, r, d] ]
-    , test [Error (Harmony 96) (Source ["p", "p2"] 1 3) "Consecutive unisons"]     $ music' [ [r, c, d, r], [r, c, d, r] ]
-    , test []                                                                      $ music' [ [c, d], [b, c, d] ]
-    , test [Error (Harmony 96) (Source ["p", "p2"] 1 3) "Consecutive unisons"]     $ music' [ [d, c, d, d], [b, c, d, b] ]
-    , test []                                                                      $ music' [ [c, d], [d, e], [c, e] ]
-    , test [Error (Harmony 96) (Source ["p", "p3"] 0 2) "Consecutive unisons"]     $ music' [ [c, d], [d, e], [c, d] ]
-    , test [Error (Harmony 96) (Source ["p", "p2"] 0 2) "Consecutive octaves"]     $ music' [ [c, d], [c', d'] ]
-    , test []                                                                      $ music' [ [c, c], [c, c] ]
-    , test []                                                                      $ music' [ [c, c], [c', c'] ]
-    , test []                                                                      $ music' [ [c, c], [c, c'] ]
-    , test []                                                                      $ music' [ [c, c], [c, d], [d, c] ]
+    [ test []                                                                      $ music' [ [g, c, d], [e, d, e] ]
+    , test []                                                                      $ music' [ [g, c, d], [e, c, e] ]
+    , test []                                                                      $ music' [ [g, c, d], [e, d, d] ]
+    , test [Error (Harmony 96) (Source ["p", "p2"] 1 3) "Consecutive unisons"]     $ music' [ [g, c, d], [e, c, d] ]
+    , test []                                                                      $ music' [ [g, c, d], [e, c, r, d] ]
+    , test [Error (Harmony 96) (Source ["p", "p2"] 2 4) "Consecutive unisons"]     $ music' [ [g, r, c, d, r], [e, r, c, d, r] ]
+    , test []                                                                      $ music' [ [g, c, d], [e, b, c, d] ]
+    , test [Error (Harmony 96) (Source ["p", "p2"] 2 4) "Consecutive unisons"]     $ music' [ [g, d, c, d, d], [e, b, c, d, b] ]
+    , test []                                                                      $ music' [ [g, c, d], [e, d, e], [e, c, e] ]
+    , test [Error (Harmony 96) (Source ["p", "p3"] 1 3) "Consecutive unisons"]     $ music' [ [g, c, d], [e, d, e], [e, c, d] ]
+    , test [Error (Harmony 96) (Source ["p", "p2"] 1 3) "Consecutive octaves"]     $ music' [ [g, c, d], [e, c', d'] ]
+    , test []                                                                      $ music' [ [g, c, c], [e, c, c] ]
+    , test []                                                                      $ music' [ [g, c, c], [e, c', c'] ]
+    , test []                                                                      $ music' [ [g, c, c], [e, c, c'] ]
+    , test []                                                                      $ music' [ [g, c, c], [e, c, d], [e, d, c] ]
+-- Parts that are nothing but consecutive octaves or unisons are allowed. However, its not worth complicating the code to handle this case at this stage
+--     , test []                                                                      $ music' [ [c, d, e], [c, d, e] ]
+--     , test []                                                                      $ music' [ [c, d, e], [c', d', e'] ]
     ] where
         test e m = show m ~: e ~=? analyse m
