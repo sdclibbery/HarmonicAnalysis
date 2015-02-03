bdf3244eee17a90ff624be634bb6eb688d4b2380
sdclibbery <sdclibbery@googlemail.com>
2015-02-03 19:57:19 +0000

    Added ear training file

---
+++ EarTraining.hs
@@ -0,0 +1,27 @@
+import Structure
+import Note
+import Interval
+import Intervals
+import Notes
+import Midi
+import Key
+import Keys
+import Data.Ratio
+import Data.List
+import Data.Ord
+import System.Random
+
+-- Output relevant files to support ear training as suggested in http://www.miles.be/articles/7-ear-training-a-direct-and-logical-path
+
+
+-- Scale then random note inside scale followed by journey back to tonic
+
+-- Same but several of note+journey before repeating scale
+
+-- Scale then random note which may be outside of scale followed by journey back to tonic
+
+-- ...
+
+
+main = do
+	createMidi "eartraining1.midi" $ music [[c, d, e, f, g, a, b]]
